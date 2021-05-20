dc8ad9d05cbf54c2cfa4698b8a1e2d9a25f6a451
CecilXIII <CecilXIII@gmail.com>
2021-05-20 11:15:39 +0700

    Implemented readData & addData to Firebase

--- app/src/main/java/org/netizencoders/kotaquest/MainActivity.kt
+++ app/src/main/java/org/netizencoders/kotaquest/MainActivity.kt
@@ -1,11 +1,56 @@
 package org.netizencoders.kotaquest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class   MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+
+    private val db = Firebase.firestore
+
+    private val user = hashMapOf(
+        "first" to "Dea",
+        "last" to "Aurora",
+        "born" to 2019
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            addData()
+            readData()
+        }
+
+    }
+
+    private fun addData() {
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("", "Error adding document", e)
+            }
+    }
+
+    private fun readData() {
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("", "Error getting documents.", exception)
+            }
     }
 }
