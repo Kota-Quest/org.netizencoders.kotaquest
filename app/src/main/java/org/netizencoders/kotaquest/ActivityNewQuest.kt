4b801b0f795026aec53e6dce40e3654279ce6135
CecilXIII <CecilXIII@gmail.com>
2021-05-27 21:37:19 +0700

    Initial commit

---
+++ app/src/main/java/org/netizencoders/kotaquest/ActivityNewQuest.kt
@@ -0,0 +1,11 @@
+package org.netizencoders.kotaquest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityNewQuest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_newquest)
+    }
+}
