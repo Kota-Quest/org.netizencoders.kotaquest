5cb25263ad267d7783c0a6b9dd3639804ee72e11
CecilXIII <CecilXIII@gmail.com>
2021-06-02 20:51:20 +0700

    Initial commit

---
+++ app/src/main/java/org/netizencoders/kotaquest/Quest.kt
@@ -0,0 +1,8 @@
+package org.netizencoders.kotaquest
+
+data class Quest(
+    var Title: String? = "",
+    var Location: String? = "",
+    var Description: String? = "",
+    var ImageURL: String? = ""
+)
