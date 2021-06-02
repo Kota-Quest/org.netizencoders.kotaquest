2d9c546cc2353d4c613e4549a45fa5127aa76d82
CecilXIII <CecilXIII@gmail.com>
2021-06-02 20:51:20 +0700

    Flow control

--- app/src/main/java/org/netizencoders/kotaquest/ActivityLogin.kt
+++ app/src/main/java/org/netizencoders/kotaquest/ActivityLogin.kt
@@ -1,11 +1,21 @@
 package org.netizencoders.kotaquest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ActivityLogin : AppCompatActivity() {
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button = findViewById(R.id.login)
+        button.setOnClickListener {
+            val moveIntent = Intent(this, ActivityListQuest::class.java)
+            startActivity(moveIntent)
+        }
     }
 }
