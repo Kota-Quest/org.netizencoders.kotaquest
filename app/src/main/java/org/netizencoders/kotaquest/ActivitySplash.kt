67211eef28720cda266fa5a78ee49748670c0af9
CecilXIII <CecilXIII@gmail.com>
2021-06-03 20:52:05 +0700

    Aesthetic change

--- app/src/main/java/org/netizencoders/kotaquest/ActivitySplash.kt
+++ app/src/main/java/org/netizencoders/kotaquest/ActivitySplash.kt
@@ -1,10 +1,14 @@
 package org.netizencoders.kotaquest
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class ActivitySplash : AppCompatActivity() {
     private var timeout:Long = 2000
@@ -16,6 +20,12 @@ class ActivitySplash : AppCompatActivity() {
         val actionBar = supportActionBar
         actionBar?.hide()
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.WHITE
+        }
+
         loadSplashScreen()
     }
 
@@ -23,6 +33,7 @@ class ActivitySplash : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this,ActivityLogin::class.java)
             startActivity(intent)
+            overridePendingTransition(R.anim.transition_bottom_up, R.anim.nothing)
             finish()
         }, timeout)
     }
