3af733cc1077d07997295698c85cf358d43702ee
CecilXIII <CecilXIII@gmail.com>
2021-06-03 18:15:46 +0700

    Deleted file

--- app/src/main/java/org/netizencoders/kotaquest/Quest.kt
+++
@@ -1,9 +0,0 @@
-package org.netizencoders.kotaquest
-
-data class Quest(
-    var ID: String? = "",
-    var Title: String? = "",
-    var Location: String? = "",
-    var Description: String? = "",
-    var ImageURL: String? = ""
-)
